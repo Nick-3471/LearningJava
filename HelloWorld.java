3b9865ebef6f29f4f72999a6d2266380148a0afc
Nick Smith <nicbrsmith@gmail.com>
2020-04-13 22:20:29 -0700

    Working on Primative Data Types

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,20 @@
 
 public class HelloWorld {
-    
+
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        int number = 5+5;
+        int twelve = 12;
+        int six = 6;
+
+        int total = number + twelve + six;
+        int negativeTotal = total - 1000;
+
+        System.out.println(number+twelve+six);
+        System.out.println(total);
+        System.out.println(negativeTotal);
+
     }
 
 

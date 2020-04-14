f74d7f647c51711b005e1b2828faada261d6c832
Nick Smith <nicbrsmith@gmail.com>
2020-04-13 22:47:05 -0700

    Working with Float/Double

--- Primative/ByteShortIntLong.java
+++ Primative/ByteShortIntLong.java
@@ -9,36 +9,38 @@ public class ByteShortIntLong {
         int myValue = 10_000;
         int minValue = Integer.MIN_VALUE;
         int maxValue = Integer.MAX_VALUE;
-
-        System.out.println(minValue);
-        System.out.println(maxValue);
-        System.out.println(myValue);
+            System.out.println(minValue);
+            System.out.println(maxValue);
+            System.out.println(myValue);
 
         //Byte Stuff
         byte byteMinValue = Byte.MIN_VALUE;
         byte byteMaxValue = Byte.MAX_VALUE;
-
-        System.out.println(byteMinValue);
-        System.out.println(byteMaxValue);
-
-
+            System.out.println(byteMinValue);
+            System.out.println(byteMaxValue);
 
         //short Stuff
         short shortMinValue = Short.MIN_VALUE;
         short shortMaxValue = Short.MAX_VALUE;
-
-        System.out.println(shortMinValue);
-        System.out.println(shortMaxValue);
+            System.out.println(shortMinValue);
+            System.out.println(shortMaxValue);
 
         //long Stuff
+        long myLongVaule = 2_147_483_648L;
         long longMinValue = Long.MIN_VALUE;
         long longMaxValue = Long.MAX_VALUE;
+            System.out.println(myLongVaule);
+            System.out.println(longMinValue);
+            System.out.println(longMaxValue);
 
-        System.out.println(longMinValue);
-        System.out.println(longMaxValue);
+        //Type Casting Variables
+        byte myNewByte = (byte)(byteMaxValue/2);
+        short myNewShort = (short)(shortMaxValue/2);
+        long myNewLong = (myLongVaule /2);
+
+        byte myByte = 26;
+        short myShort = 25497;
+        long testLong = (50_000 + (10*(myValue + myShort + myByte)));
+        System.out.println(testLong);
     }
-
-
-    
-
 }
---
+++ Primative/FlaotDouble.java
@@ -0,0 +1,19 @@
+package Primative;
+
+public class FlaotDouble {
+
+    public static void main(String[] args) {
+    
+    //Float Stuff
+    float floatMinValue = Float.MIN_VALUE;
+    float floatMaxValue = Float.MAX_VALUE;
+        System.out.println(floatMinValue);
+        System.out.println(floatMaxValue);
+
+    //Double Stuff
+    double doubleMinValue = Double.MIN_VALUE;
+    double doubleMaxValue = Double.MAX_VALUE;
+        System.out.println(doubleMinValue);
+        System.out.println(doubleMaxValue);
+    }
+}

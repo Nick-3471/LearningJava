6a0238008e73a15063b228de4e1a5623a0c13db0
Nick Smith <nicbrsmith@gmail.com>
2020-04-13 22:23:33 -0700

    Working with Primatives

---
+++ Primative/ByteShortIntLong.java
@@ -0,0 +1,44 @@
+package Primative;
+
+public class ByteShortIntLong {
+
+    public static void main(String[] args) {
+        
+
+        //Integer Stuff
+        int myValue = 10_000;
+        int minValue = Integer.MIN_VALUE;
+        int maxValue = Integer.MAX_VALUE;
+
+        System.out.println(minValue);
+        System.out.println(maxValue);
+        System.out.println(myValue);
+
+        //Byte Stuff
+        byte byteMinValue = Byte.MIN_VALUE;
+        byte byteMaxValue = Byte.MAX_VALUE;
+
+        System.out.println(byteMinValue);
+        System.out.println(byteMaxValue);
+
+
+
+        //short Stuff
+        short shortMinValue = Short.MIN_VALUE;
+        short shortMaxValue = Short.MAX_VALUE;
+
+        System.out.println(shortMinValue);
+        System.out.println(shortMaxValue);
+
+        //long Stuff
+        long longMinValue = Long.MIN_VALUE;
+        long longMaxValue = Long.MAX_VALUE;
+
+        System.out.println(longMinValue);
+        System.out.println(longMaxValue);
+    }
+
+
+    
+
+}

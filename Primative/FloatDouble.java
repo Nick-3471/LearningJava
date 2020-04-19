76f3be3736d40d519ae9b18bc8f7c3939aab0d2c
Nick Smith <nicbrsmith@gmail.com>
2020-04-19 14:15:04 -0700

    Renamed File

---
+++ Primative/FloatDouble.java
@@ -0,0 +1,27 @@
+package Primative;
+
+public class FloatDouble {
+
+    public static void main(String[] args) {
+    
+    //Float Stuff
+    float floatMinValue = Float.MIN_VALUE;
+    float floatMaxValue = Float.MAX_VALUE;
+        System.out.println(floatMinValue);
+        System.out.println(floatMaxValue);
+
+    //Double Stuff
+    double doubleMinValue = Double.MIN_VALUE;
+    double doubleMaxValue = Double.MAX_VALUE;
+        System.out.println(doubleMinValue);
+        System.out.println(doubleMaxValue);
+
+
+
+        int myInt = 5;
+        float myFloat = 5.25f;
+        double myDouble = 5.25d;
+
+
+    }
+}
